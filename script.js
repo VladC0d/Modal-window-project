19aefce1a0d83489a3fed56feadfd51091ec0ad8
C0d <73534257+VladC0d@users.noreply.github.com>
2022-06-23 15:56:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+//Thats how i did it
+// document.addEventListener('keydown', function (e) {
+//   if (e.key === 'Escape') {
+//     closeModal();
+//   }
+// });
+
+//Thats how he did it
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) closeModal();
+  }
+});
